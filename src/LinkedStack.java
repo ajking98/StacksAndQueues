3fe4047fd2114b7c6e6334ce8181ff93ac106f37
ajking98 <agedi98@gmail.com>
2017-09-04 11:49:10 -0400

    first commit

---
+++ src/LinkedStack.java
@@ -0,0 +1,49 @@
+/**
+ * Your implementation of a linked stack.
+ *
+ * @author YOUR NAME HERE
+ * @userid YOUR USER ID HERE (i.e. gburdell3)
+ * @GTID YOUR GT ID HERE (i.e. 900000000)
+ * @version 1.0
+ */
+public class LinkedStack<T> implements StackInterface<T> {
+
+    // Do not add new instance variables.
+    private LinkedNode<T> head;
+    private int size;
+
+    @Override
+    public T pop() {
+
+    }
+
+    @Override
+    public void push(T data) {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // DO NOT MODIFY THIS METHOD!
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
+
+    /**
+     * Returns the head of this stack.
+     * Normally, you would not do this, but we need it for grading your work.
+     *
+     * DO NOT USE THIS METHOD IN YOUR CODE.
+     *
+     * @return the head node
+     */
+    public LinkedNode<T> getHead() {
+        // DO NOT MODIFY THIS METHOD!
+        return head;
+    }
+}
